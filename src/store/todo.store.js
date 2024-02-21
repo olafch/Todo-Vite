cf44d81f0c9b5518f4040a6448f0b9f1b1ab4b43
Olaf Chwolka <olaf.chwolka@outlook.com>
2024-02-21 14:09:57 +0100

    Implement methods in our Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,48 @@ const initStore = () => {
   console.log("InitStore 🥑");
 };
 
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
+
+/**
+ *
+ * @param {String} description
+ */
+const addTodo = (description) => {
+  throw new Error("Not implemented");
+};
+
+/**
+ *
+ * @param {String} todoId Todo identifier
+ */
+const toggleTodo = (todoId) => {
+  throw new Error("Not implemented");
+};
+
+const deleteTodo = (todoId) => {
+  throw new Error("Not implemented");
+};
+
+const deleteCompleted = () => {
+  throw new Error("Not implemented");
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error("Not implemented");
+};
+
+const getFilter = () => {
+  throw new Error("Not implemented");
+};
+
 export default {
+  deleteCompleted,
+  deleteTodo,
+  getFilter,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 };
