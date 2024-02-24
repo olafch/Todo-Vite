1d3996c90c2488aa4d8005c7261e4f95d005ca17
Olaf Chwolka <olaf.chwolka@outlook.com>
2024-02-24 22:31:41 +0100

    Add buttons functioning

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,6 +1,6 @@
 import { Todo } from "../todos/models/todo.model";
 
-const Filters = {
+export const Filters = {
   All: "All",
   Completed: "Completed",
   Pending: "Deleted",
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,11 +1,12 @@
 import html from "./app.html?raw";
-import todoStore from "../store/todo.store";
+import todoStore, { Filters } from "../store/todo.store";
 import { renderTodos } from "./use-cases";
 
 const ElementIDs = {
   ClearCompletedButton: ".clear-completed",
   TodoList: ".todo-list",
   NewTodoInput: "#new-todo-input",
+  TodoFilters: ".filtro",
 };
 
 /**
@@ -33,6 +34,7 @@ export const App = (elementId) => {
   const clearCompletedButton = document.querySelector(
     ElementIDs.ClearCompletedButton
   );
+  const filtersLIs = document.querySelectorAll(ElementIDs.TodoFilters);
 
   //Listeners
   newDescriptionInput.addEventListener("keyup", (event) => {
@@ -61,5 +63,24 @@ export const App = (elementId) => {
   clearCompletedButton.addEventListener("click", () => {
     todoStore.deleteCompleted();
     displayTodos();
+  });
+
+  filtersLIs.forEach((element) => {
+    element.addEventListener("click", (element) => {
+      filtersLIs.forEach((el) => el.classList.remove("selected"));
+      element.target.classList.add("selected");
+      switch (element.target.text) {
+        case "Todos":
+          todoStore.setFilter(Filters.All);
+          break;
+        case "Pendientes":
+          todoStore.setFilter(Filters.Pending);
+          break;
+        case "Completados":
+          todoStore.setFilter(Filters.Completed);
+          break;
+      }
+      displayTodos();
+    });
   });
 };
